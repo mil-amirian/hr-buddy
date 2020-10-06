8248a82db2d2bd006685083261611dbf805d5cee
Mil Amirian <milamirian22@gmail.com>
2020-10-06 00:20:02 +0000

    updated header buttons and set global states ready for each component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,10 +6,10 @@ export default function Header(props) {
       <nav className="">
         <div className="btn-toolbar justify-content-between navbar-header pt-2 pb-5 pl-2 pr-2" role="toolbar" aria-label="Toolbar with button groups">
           <div className="btn-group mr-2" role="group" aria-label="First group">
-            <button id="home-btn" type="button" className="btn btn-light">
+            <button id="home-btn" type="button" className="btn btn-light" onClick={() => { props.mainMenu('main-menu'); }}>
               <img className="home-icon" src="./images/hr-buddy-icon.png" alt=""/>
             </button>
-            <button id="view-emp-btn" type="button" className="btn btn-light ml-1">
+            <button id="view-emp-btn" type="button" className="btn btn-light ml-1" onClick={() => { props.employees('view-employees'); }}>
               <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" className="bi bi-person-lines-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fillRule="evenodd" d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm2 9a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"/>
               </svg>
