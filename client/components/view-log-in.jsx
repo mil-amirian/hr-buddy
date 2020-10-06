eaabf0049979f3177ed5eff27706aff4671ea920
andrewkyungwonpark <andrewkpark@yahoo.com>
2020-10-06 10:59:18 -0700

    Added handleChange function

--- client/components/view-log-in.jsx
+++ client/components/view-log-in.jsx
@@ -4,15 +4,22 @@ class LogIn extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      employees: []
+      employees: [],
+      currentUser: {
+        firstName: null,
+        lastName: null
+      }
     };
-
     this.getEmployees = this.getEmployees.bind(this);
-    this.hanldeChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     this.getEmployees();
+  }
+
+  handleChange(event) {
+    this.props.getCurrentUser(event.target.value);
   }
 
   getEmployees() {
@@ -39,10 +46,10 @@ class LogIn extends React.Component {
               <div className="input-group-prepend">
                 <label className="input-group-text" htmlFor="employee-select">Select User</label>
               </div>
-              <select className="custom-select" id="employee-select" defaultValue="--select--">
+              <select className="custom-select" id="employee-select" onChange={this.handleChange}>
                 {this.state.employees.map(employee => {
                   return (
-                    <option key={employee.firstName} onChange={this.handleChange}>{employee.firstName} {employee.lastName}</option>
+                    <option key={employee.firstName}>{employee.firstName} {employee.lastName}</option>
                   );
                 })
                 }
