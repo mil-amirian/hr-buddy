4e9afd96aaee0c09db5c7608b3bf95c48865534a
Mil Amirian <milamirian22@gmail.com>
2020-10-08 17:45:39 +0000

    updated UI on view departments page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -108,7 +108,7 @@ export default class App extends React.Component {
         return (
           <>
             {header}
-            <ViewDepartments/>
+            <ViewDepartments setView={this.setView}/>
           </>
         );
     }
