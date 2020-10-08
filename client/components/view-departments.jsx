917478984fd46bcac40229b4f4c8d7cc44a1b275
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-10-08 17:05:06 +0000

    frontend-departments-features

--- client/components/view-departments.jsx
+++ client/components/view-departments.jsx
@@ -24,17 +24,39 @@ export default class ViewDepartments extends React.Component {
   }
 
   render() {
-    return (
-      <main className="d-flex justify-content-center mb-4">
-        <div className="page-content col-10">
-          <div className="title d-flex justify-content-center">
-            <h2 className="page-title">View Employees</h2>
+    if (this.state.departments) {
+      return (
+        <>
+          <main className="d-flex justify-content-center mb-4">
+            <div className="page-content col-10">
+              <div className="title d-flex justify-content-center">
+                <h2 className="page-title">View Employees</h2>
+              </div>
+            </div>
+          </main>
+          <div className=" col-3 d-flex">
+            {
+              this.state.departments.map(department => {
+                return (
+                  <div key={department.departmentId} className='container'>
+                    <div className="row align-items-start">
+                      <div className="col bubble">
+                        <h4 > {department.department} Departments</h4>
+                        <h6>Members in deparment {department.numbersOfPeople}</h6>
+                      </div>
+                    </div>
+                  </div>
+
+                );
+              })
+            }
           </div>
-        </div>
-      </main>
-
-    );
-
+        </>
+      );
+    } else {
+      return (
+        <h1>Loading departments...</h1>
+      );
+    }
   }
-
 }
